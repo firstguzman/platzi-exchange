551194e55d74a7bce3088c96fca2a600280b128c
César <cesar_guzman2000@hotmail.com>
2021-12-29 16:04:21 -0400

    Ref add button created

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -53,20 +53,34 @@
         >
           {{ asset.changePercent24Hr | percent }}
         </td>
-        <td class="hidden sm:block"></td>
+        <td class="hidden sm:block">
+          <px-button @click="goToCoin(asset.id)">
+            <span>Detalle</span>
+          </px-button>
+        </td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
+import PxButton from '@/components/PxButton'
+
 export default {
   name: 'PxAssetsTable',
+
+  components: { PxButton },
 
   props: {
     assets: {
       type: Array,
       default: () => []
+    }
+  },
+
+  methods: {
+    goToCoin(id) {
+      this.$router.push({ name: 'coin-detail', params: { id: id } })
     }
   }
 }
